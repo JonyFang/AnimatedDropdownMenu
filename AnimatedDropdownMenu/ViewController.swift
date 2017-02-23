c50afcafbf1afdc3f6261b434aa4a8f70bbbe741
JonyFang <ahchunfang@gmail.com>
2017-02-23 15:36:07 +0800

    INIT: Init project

---
+++ AnimatedDropdownMenu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AnimatedDropdownMenu
+//
+//  Created by JonyFang on 17/2/23.
+//  Copyright © 2017年 JonyFang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
