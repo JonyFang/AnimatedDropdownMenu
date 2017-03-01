da95b33d6bf8b91a8f96313ab0e09cd849f6cfbf
JonyFang <ahchunfang@gmail.com>
2017-03-01 17:05:03 +0800

    1.Configure AnimatedDropdownMenu in ViewController, 2.ADDED: Add dropdownItems resources

--- AnimatedDropdownMenu/ViewController.swift
+++ AnimatedDropdownMenu/ViewController.swift
@@ -9,17 +9,66 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - Properties
+    fileprivate let dropdownItems: [AnimatedDropdownMenu.Item] = [
+        AnimatedDropdownMenu.Item(
+            title: "From | Photography",
+            icon: UIImage(named: "icon_unsplash_gray")!,
+            iconLight: UIImage(named: "icon_unsplash_light")!
+        ),
+        AnimatedDropdownMenu.Item(
+            title: "From | Artwork",
+            icon: UIImage(named: "icon_artand_gray")!,
+            iconLight: UIImage(named: "icon_artand_light")!
+        ),
+        AnimatedDropdownMenu.Item(
+            title: "Others",
+            icon: UIImage(named: "icon_other_gray")!,
+            iconLight: UIImage(named: "icon_other_light")!
+        )
+    ]
+    
+    fileprivate var selectedStageIndex: Int = 0
+    fileprivate var lastStageIndex: Int = 0
+    
+    // MARK: - Life Cycle
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setupAnimatedDropdownMenu()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // MARK: - Private Methods
+    
+    fileprivate func setupAnimatedDropdownMenu() {
+        
+        let dropdownMenu = AnimatedDropdownMenu(navigationController: navigationController, containerView: view, selectedIndex: selectedStageIndex, items: dropdownItems)
+        dropdownMenu.didSelectItemAtIndexHandler = {
+            [weak self] selectedIndex in
+            
+            guard let strongSelf = self else {
+                return
+            }
+            
+            strongSelf.lastStageIndex = strongSelf.selectedStageIndex
+            strongSelf.selectedStageIndex = selectedIndex
+            
+            guard strongSelf.selectedStageIndex != strongSelf.lastStageIndex else {
+                return
+            }
+            
+            //Configure Selected Action
+            strongSelf.selectedAction()
+        }
+        
+        navigationItem.titleView = dropdownMenu
     }
-
-
+    
+    private func selectedAction() {
+        print("\(dropdownItems[selectedStageIndex].title)")
+    }
+    
 }
 
