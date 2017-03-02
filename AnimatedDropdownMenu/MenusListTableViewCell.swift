8048d6ac50fddad53c07c8dafd9a763acb155cbd
JonyFang <ahchunfang@gmail.com>
2017-03-02 19:28:28 +0800

    ADDED: Add MenusListTableViewCell

---
+++ AnimatedDropdownMenu/MenusListTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  MenusListTableViewCell.swift
+//  AnimatedDropdownMenu
+//
+//  Created by JonyFang on 17/3/2.
+//  Copyright © 2017年 JonyFang. All rights reserved.
+//
+
+import UIKit
+
+class MenusListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
