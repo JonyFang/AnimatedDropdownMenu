fd7894ba3df41ebb0989ac7a18065897dddbe90a
JonyFang <ahchunfang@gmail.com>
2017-03-02 19:34:13 +0800

    Add ListCell class methods reuseIdentifier() & requireCellHeight()

--- AnimatedDropdownMenu/MenusListTableViewCell.swift
+++ AnimatedDropdownMenu/MenusListTableViewCell.swift
@@ -9,6 +9,15 @@
 import UIKit
 
 class MenusListTableViewCell: UITableViewCell {
+    
+    public class var reuseIdentifier: String {
+        return "\(self)"
+    }
+    
+    public class var requireCellHeight: CGFloat {
+        let screenWidth = UIScreen.main.bounds.width
+        return screenWidth / 375.0 * 184.0
+    }
 
     override func awakeFromNib() {
         super.awakeFromNib()
